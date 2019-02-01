8b9d2d25ad3c722dc0776472c1653c92d2b0faa4
Labop2 <labop2@laboratoire.uqac.ca>
2019-02-01 15:25:48 -0500

    Preuve de concept

--- app/src/main/java/proj/myapplication/MainActivity.java
+++ app/src/main/java/proj/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -23,8 +24,12 @@ public class MainActivity extends AppCompatActivity {
     private Button connectBtn;
     private Spinner mySpinner;
     private TextView textView;
+    private EditText editText;
+    private Button sendBtn;
+
     private Set<BluetoothDevice> pairedDevices;
     public ArrayAdapter<String> adapter;
+    private BluetoothSocket btSocket;
 
     private View.OnClickListener searchBtnListener = new View.OnClickListener() {
         @Override
@@ -36,7 +41,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             connectBtnClicked();
+        }
+    };
 
+    private View.OnClickListener sendBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            sendBtnClicked();
         }
     };
 
@@ -55,6 +66,11 @@ public class MainActivity extends AppCompatActivity {
 
         mySpinner = (Spinner)findViewById(R.id.spinner);
         mySpinner.setAdapter(adapter);
+
+        editText = (EditText)findViewById(R.id.editText);
+
+        sendBtn = (Button)findViewById(R.id.button_send);
+        sendBtn.setOnClickListener(sendBtnListener);
 
         textView = (TextView)findViewById(R.id.textView);
 
@@ -79,11 +95,9 @@ public class MainActivity extends AppCompatActivity {
                 // There are no paired devices. We then try to discover available devices nearby
             }
         }
-
     }
 
     private void connectBtnClicked() {
-        BluetoothSocket btSocket;
         BluetoothDevice device = getSelectedSpinnerItem();
         UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"); //Standard SerialPortService ID
         try {
@@ -98,6 +112,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void sendBtnClicked() {
+        try {
+            OutputStream btOutputStream = btSocket.getOutputStream();
+            btOutputStream.write(editText.getText().toString().getBytes());
+            btSocket.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private BluetoothDevice getSelectedSpinnerItem() {
         for (BluetoothDevice device : pairedDevices) {
            if(device.getName().equals(mySpinner.getSelectedItem().toString())) {
@@ -105,16 +130,5 @@ public class MainActivity extends AppCompatActivity {
            }
         }
         return null;
-    }
-
-    private void sendBtMsg(String msgToSend, BluetoothSocket btSocket) {
-        try {
-            OutputStream btOutputStream = btSocket.getOutputStream();
-            btOutputStream.write(msgToSend.getBytes());
-            btSocket.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
